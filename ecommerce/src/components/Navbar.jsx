c0e3d28304f2f20dc18ce9b65f868d9a76d69878
Harsh Garg <hg8728@srmist.edu.in>
2025-06-08 15:47:42 +0530

    icon changes

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -120,7 +120,7 @@ const Navbar = () => {
 
 				<div className="flex items-center gap-4">
 					
-						<div className="flex flex-row gap-4 items-center min-w-[40px]">
+						<div className="sm:flex flex-row sm:gap-4 items-center ">
 							<p className="text-gray-800 font-semibold hidden sm:block w-[130px] truncate overflow-hidden whitespace-nowrap">
 								Hey, {token ? userName : "User"}!
 							</p>
