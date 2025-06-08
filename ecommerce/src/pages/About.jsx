80a5c5b00091b42b4a5254ad20cdba61a6ef99db
Harsh Garg <hg8728@srmist.edu.in>
2025-06-08 15:28:21 +0530

    size updated for the about

--- ecommerce/src/pages/About.jsx
+++ ecommerce/src/pages/About.jsx
@@ -1,13 +1,13 @@
 const About = () => {
   return (
-		<div className='flex flex-col items-center justify-center w-full h-[800px]'>
+		<div className="flex flex-col items-center justify-center ">
 			<img
 				src="about.jpg"
 				alt="About Us"
-				className="w-full h-[800px] object-cover mt-10"
+				className=" object-cover w-[75%]"
 				onError={(e) => {
-					e.target.style.display = 'none';
-					console.log('Image failed to load');
+					e.target.style.display = "none";
+					console.log("Image failed to load");
 				}}
 			/>
 		</div>
